ea294f550ffabc8c4304af7602fe873c5a3583ee
cidkdXiao <cidkdxiao@foxmail.com>
2025-05-11 17:13:59 +0800

    test

---
+++ func_test.py
@@ -0,0 +1,24 @@
+import collections
+
+# Definition for singly-linked list.
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        # 
+
+
+
+
+S = Solution()
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+result = S.rotate(matrix)
+print(result)
